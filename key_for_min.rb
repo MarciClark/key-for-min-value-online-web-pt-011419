48dc37fcfb501a70fd93219efe0b21d830ba7228
Marci Clark <marcig612@gmail.com>
2019-01-27 04:11:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,14 @@
 
 ikea = {:chair => 25 :table => 85, :mattress = > 450}
 
-def key_for_min_value(ikea)
-
-end
+def key_for_min_value(name_hash)
+  lowest_value = 0
+  lowest_key = nil
+  
+  name_hash.each do |key,value|
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
